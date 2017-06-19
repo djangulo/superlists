e2878da67f140a878792228ed0ddde9149b65bd5
Denis Angulo <denis.angulo@linekode.com>
2017-06-19 12:43:16 -0400

    minimal user model complete!
    Signed-off-by: Denis Angulo <denis.angulo@linekode.com>

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    is_anonymous = False
+    is_authenticated = True
+
+
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(default=uuid.uuid4, max_length=40)
